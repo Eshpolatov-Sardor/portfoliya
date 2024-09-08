303184dd62dbeecffd349458322e5cbde465e6c6
Eshpolatov-Sardor <167778318+Eshpolatov-Sardor@users.noreply.github.com>
2024-09-08 22:40:18 +0500

    sardor

---
+++ main.js
@@ -0,0 +1,24 @@
+import './style.css'
+import Experience from './Experience/Experience.js';
+
+const experience = new Experience(document.querySelector(".experience-canvas"));
+const time = document.querySelector("#time");
+const timeFormat = document.querySelector("#timeFormat");
+
+document.addEventListener("DOMContentLoaded", () => {
+  setInterval(showTime, 1000);
+});
+
+const showTime = () => {
+  let date = new Date();
+  let hours = date.getHours();
+  let mins = date.getMinutes();
+  let secs = date.getSeconds();
+
+  hours = hours < 10 ? `0${hours}` : hours;
+  mins = mins < 10 ? `0${mins}` : mins;
+  secs = secs < 10 ? `0${secs}` : secs;
+
+  time.innerHTML = `${hours} : ${mins} : ${secs}`;
+  timeFormat.innerHTML = hours > 12 ? "AM" : "PM";
+};
